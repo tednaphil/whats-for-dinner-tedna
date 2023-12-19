0406c67e90b1182df2bef9638ed54976c17b9f71
Tayla Phillips <richardstayla@gmail.com>
2023-12-18 22:30:19 -0600

    Add data model and querySelector variables

---
+++ main.js
@@ -0,0 +1,54 @@
+var sides = [
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Sweet Potato Tots',
+    'Coconut Rice',
+    'Caeser Salad',
+    'Shrimp Summer Rolls',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies'
+];
+
+var mains = [
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza'
+];
+
+var desserts = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs',
+];
+
+var letsCookButton = document.querySelector('#lets-cook-button');
+var clearButton = document.querySelector('#clear-button');
+var cookpot = docuemnt.querySelector('#pot-image');
+var dishDisplay = document.querySelector('#dish-rec');
+var formCatSelected = document.querySelector('input[name="radio"]:checked').value;
